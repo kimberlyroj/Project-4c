4ef1b3d6426f749900246d690bb0c41cae9790e5
kimberlyroj <90664797+kimberlyroj@users.noreply.github.com>
2022-01-28 23:53:33 -0800

    .

--- hailstone.py
+++ hailstone.py
@@ -1,7 +1,7 @@
-# Author: Kimberly Rojas
-# GitHub username: kimberlyroj
-# Date: 1/19/2021
-# Description:A function named hailstone that takes a positive integer parameter as the initial number of a hailstone sequence and returns how many steps it takes to reach 1
+#Author: Kimberly Rojas
+#GitHub username: kimberlyroj
+#Date: 1/19/2021
+#Description:A function named hailstone that takes a positive integer parameter as the initial number of a hailstone sequence and returns how many steps it takes to reach 1
 def hailstone(num):
     count = 1
     """Print the terms of 'hailstone sequence' from n to 1."""
